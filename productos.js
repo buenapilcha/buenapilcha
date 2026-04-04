0bcc94631916f25936a9ef0891e70240c1c21029
buenapilcha <buenapilcha.com@gmail.com>
2026-04-03 22:16:54 -0300

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,51 @@
+const productos = {
+
+  remeras: [
+    { titulo: "Remera Nike", descripcion: "Alta calidad", precio: "$3000", imagenes: ["remeras/fotos/nike.jpg"] },
+    { titulo: "Remera Adidas", descripcion: "Original", precio: "$3200", imagenes: ["remeras/fotos/adidas.jpg"] },
+    { titulo: "Remera Puma", descripcion: "Facherita", precio: "$2800", imagenes: ["remeras/fotos/puma.jpg"] },
+    { titulo: "Remera Oversize", descripcion: "Moda urbana", precio: "$3500", imagenes: ["remeras/fotos/oversize.jpg"] },
+    { titulo: "Remera Negra", descripcion: "Clásica", precio: "$2500", imagenes: ["remeras/fotos/negra.jpg"] }
+  ],
+
+  pantalones: [
+    { titulo: "Jean Azul", descripcion: "Slim fit", precio: "$5000", imagenes: ["pantalones/fotos/jean.jpg"] },
+    { titulo: "Pantalón Negro", descripcion: "Elegante", precio: "$4800", imagenes: ["pantalones/fotos/negro.jpg"] },
+    { titulo: "Jogger Gris", descripcion: "Cómodo", precio: "$4200", imagenes: ["pantalones/fotos/jogger.jpg"] },
+    { titulo: "Cargo Verde", descripcion: "Urbano", precio: "$5500", imagenes: ["pantalones/fotos/cargo.jpg"] },
+    { titulo: "Short Deportivo", descripcion: "Ligero", precio: "$3000", imagenes: ["pantalones/fotos/short.jpg"] }
+  ],
+
+  buzos: [
+    { titulo: "Buzo Nike", descripcion: "Abrigo premium", precio: "$6000", imagenes: ["buzos/fotos/nike.jpg"] },
+    { titulo: "Buzo Adidas", descripcion: "Clásico", precio: "$5800", imagenes: ["buzos/fotos/adidas.jpg"] },
+    { titulo: "Hoodie Negro", descripcion: "Con capucha", precio: "$6200", imagenes: ["buzos/fotos/hoodie.jpg"] },
+    { titulo: "Buzo Oversize", descripcion: "Streetwear", precio: "$6500", imagenes: ["buzos/fotos/oversize.jpg"] },
+    { titulo: "Buzo Gris", descripcion: "Minimalista", precio: "$5500", imagenes: ["buzos/fotos/gris.jpg"] }
+  ],
+
+  camperas: [
+    { titulo: "Campera Nike", descripcion: "Impermeable", precio: "$9000", imagenes: ["camperas/fotos/nike.jpg"] },
+    { titulo: "Campera Adidas", descripcion: "Deportiva", precio: "$8500", imagenes: ["camperas/fotos/adidas.jpg"] },
+    { titulo: "Campera Jean", descripcion: "Clásica", precio: "$8000", imagenes: ["camperas/fotos/jean.jpg"] },
+    { titulo: "Campera Cuero", descripcion: "Premium", precio: "$12000", imagenes: ["camperas/fotos/cuero.jpg"] },
+    { titulo: "Campera Inflable", descripcion: "Invierno", precio: "$10000", imagenes: ["camperas/fotos/inflable.jpg"] }
+  ],
+
+  accesorios: [
+    { titulo: "Zapatillas Nike", descripcion: "Comodidad", precio: "$12000", imagenes: ["zapatillas/fotos/nike.jpg"] },
+    { titulo: "Zapatillas Adidas", descripcion: "Deportivas", precio: "$11500", imagenes: ["zapatillas/fotos/adidas.jpg"] },
+    { titulo: "Zapatillas Puma", descripcion: "Urbanas", precio: "$11000", imagenes: ["zapatillas/fotos/puma.jpg"] },
+    { titulo: "Zapatillas Running", descripcion: "Livianas", precio: "$13000", imagenes: ["zapatillas/fotos/running.jpg"] },
+    { titulo: "Zapatillas Casual", descripcion: "Diario", precio: "$10000", imagenes: ["zapatillas/fotos/casual.jpg"] }
+  ],
+
+  bicicletas: [
+    { titulo: "Gorra Nike", descripcion: "Ajustable", precio: "$2000", imagenes: ["gorras/fotos/nike.jpg"] },
+    { titulo: "Gorra Adidas", descripcion: "Clásica", precio: "$2200", imagenes: ["gorras/fotos/adidas.jpg"] },
+    { titulo: "Gorra Puma", descripcion: "Moderna", precio: "$2100", imagenes: ["gorras/fotos/puma.jpg"] },
+    { titulo: "Gorra Negra", descripcion: "Minimal", precio: "$1800", imagenes: ["gorras/fotos/negra.jpg"] },
+    { titulo: "Gorra Blanca", descripcion: "Verano", precio: "$1800", imagenes: ["gorras/fotos/blanca.jpg"] }
+  ]
+
+};
